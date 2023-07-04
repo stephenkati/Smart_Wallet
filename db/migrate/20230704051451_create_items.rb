117c405078bd186970030b71afcc2e1690fe0f31
stephenkati <stephenkatuli88@gmail.com>
2023-07-04 08:23:34 +0300

    update migrations

--- db/migrate/20230704051451_create_items.rb
+++ db/migrate/20230704051451_create_items.rb
@@ -3,7 +3,7 @@ class CreateItems < ActiveRecord::Migration[7.0]
     create_table :items do |t|
       t.string :name
       t.integer :amount
-      t.references :user, null: false, foreign_key: true
+      t.references :author, null: false, foreign_key: { to_table: :users }
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_04_024322) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_04_052143) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -26,10 +26,10 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_04_024322) do
   create_table "items", force: :cascade do |t|
     t.string "name"
     t.integer "amount"
-    t.bigint "user_id", null: false
+    t.bigint "author_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_items_on_user_id"
+    t.index ["author_id"], name: "index_items_on_author_id"
   end
 
   create_table "payments", force: :cascade do |t|
@@ -50,7 +50,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_04_024322) do
   end
 
   add_foreign_key "categories", "users"
-  add_foreign_key "items", "users"
+  add_foreign_key "items", "users", column: "author_id"
   add_foreign_key "payments", "categories"
   add_foreign_key "payments", "items"
 end
