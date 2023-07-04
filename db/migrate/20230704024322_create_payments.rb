c219a54fd06d64eac2c767da47286d2205925897
stephenkati <stephenkatuli88@gmail.com>
2023-07-04 06:18:45 +0300

    rename migration

--- db/migrate/20230704024322_create_payments.rb
+++ db/migrate/20230704024322_create_payments.rb
@@ -2,7 +2,7 @@ class CreatePayments < ActiveRecord::Migration[7.0]
   def change
     create_table :payments do |t|
       t.references :item, null: false, foreign_key: true
-      t.references :catogory, null: false, foreign_key: true
+      t.references :category, null: false, foreign_key: true
 
       t.timestamps
     end
