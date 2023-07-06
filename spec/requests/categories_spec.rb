4d900244985132e27a224b842389f7aab0287a51
stephenkati <stephenkatuli88@gmail.com>
2023-07-06 13:54:14 +0300

    update tests

--- spec/requests/categories_spec.rb
+++ spec/requests/categories_spec.rb
@@ -1,19 +1,27 @@
-# categories_controller_spec.rb
-
 require 'rails_helper'
 
 RSpec.describe CategoriesController, type: :request do
+  let(:user) { User.create(name: 'John Doe', email: 'user@example.com', password: 'password') }
+
+  def authenticate_user(user)
+    post user_session_path, params: { user: { email: user.email, password: user.password, name: user.name } }
+    follow_redirect!
+  end
+
   describe 'GET /index' do
     it 'returns a successful response' do
-      get '/categories'
+      authenticate_user(user)
+      get categories_path
+
       expect(response).to have_http_status(:success)
     end
   end
 
   describe 'GET /show' do
-    let(:category) { Category.create(name: 'my category', icon: 'icon', user_id: 1) }
+    let(:category) { Category.create(name: 'my category', icon: 'icon', user_id: user.id) }
 
     it 'returns a successful response' do
+      authenticate_user(user)
       get "/categories/#{category.id}"
       expect(response).to have_http_status(:success)
     end
@@ -21,6 +29,7 @@ RSpec.describe CategoriesController, type: :request do
 
   describe 'GET /new' do
     it 'returns a successful response' do
+      authenticate_user(user)
       get '/categories/new'
       expect(response).to have_http_status(:success)
     end
@@ -30,47 +39,50 @@ RSpec.describe CategoriesController, type: :request do
     let(:category_params) { { category: { name: 'my category', icon: 'icon' } } }
 
     it 'creates a new category' do
+      authenticate_user(user)
       expect do
         post '/categories', params: category_params
       end.to change(Category, :count).by(1)
     end
 
     it 'redirects to the categories index' do
+      authenticate_user(user)
       post '/categories', params: category_params
       expect(response).to redirect_to(categories_path)
     end
   end
 
-  # describe 'GET /edit' do
-  #   let!(:category) { Category.create(name: 'my category', icon: 'icon', user_id: 1) }
+  describe 'GET /edit' do
+    let!(:category) { Category.create(name: 'my category', icon: 'icon', user_id: user.id) }
 
-  #   it 'returns a successful response' do
-  #     get "/categories/#{category.id}/edit"
-  #     expect(response).to have_http_status(:success)
-  #   end
-  # end
+    it 'returns a successful response' do
+      authenticate_user(user)
+      get "/categories/#{category.id}/edit"
+      expect(response).to have_http_status(:success)
+    end
+  end
 
-  # describe 'PUT /update' do
-  #   let!(:category) { Category.create(name: 'my category', icon: 'icon', user_id: 1) }
-  #   let(:updated_category_params) { { category: { name: 'updated category', icon: 'updated icon' } } }
+  describe 'PUT /update' do
+    let!(:category) { Category.create(name: 'my category', icon: 'icon', user_id: user.id) }
+    let(:updated_category_params) { { category: { name: 'updated category', icon: 'updated icon' } } }
 
-  #   it 'updates the category' do
-  #     put "/categories/#{category.id}", params: updated_category_params
-  #     category.reload
-  #     expect(category.name).to eq('updated category')
-  #     expect(category.icon).to eq('updated icon')
-  #   end
+    it 'updates the category' do
+      authenticate_user(user)
+      put "/categories/#{category.id}", params: updated_category_params
+      category.reload
+      expect(category.name).to eq('updated category')
+      expect(category.icon).to eq('updated icon')
+    end
+  end
 
-  # end
+  describe 'DELETE /destroy' do
+    let!(:category) { Category.create(name: 'my category', icon: 'icon', user_id: user.id) }
 
-  # describe 'DELETE /destroy' do
-  #   let!(:category) { Category.create(name: 'my category', icon: 'icon', user_id: 1) }
-
-  #   it 'destroys the category' do
-  #     expect {
-  #       delete "/categories/#{category.id}"
-  #     }.to change(Category, :count).by(-1)
-  #   end
-
-  # end
+    it 'destroys the category' do
+      authenticate_user(user)
+      expect do
+        delete "/categories/#{category.id}"
+      end.to change(Category, :count).by(-1)
+    end
+  end
 end
