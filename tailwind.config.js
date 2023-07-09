783bdf273328ce7db5adc2568d87c10b915e7295
stephenkati <stephenkatuli88@gmail.com>
2023-07-09 16:37:47 +0300

    add tailwind config to root

---
+++ tailwind.config.js
@@ -0,0 +1,61 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  content: [
+    './public/*.html',
+    './app/helpers/**/*.rb',
+    './app/javascript/**/*.js',
+    './app/views/**/*.{erb,haml,html,slim}'
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/container-queries'),
+    require("daisyui"),
+  ],
+  // daisyui: {
+  //   themes: ["lemonade"],
+  //   // darkTheme: "dark",
+  // }
+  daisyui: {
+    themes: [
+      {
+        'mytheme': {                          /* your theme name */
+           'primary' : '#3778c2',           /* Primary color */
+           'primary-focus' : '#1252A6',     /* Primary color - focused */
+           'primary-content' : '#ffffff',   /* Foreground content color to use on primary color */
+
+           'secondary' : '#5fb523',         /* Secondary color */
+           'secondary-focus' : '#4D9600',   /* Secondary color - focused */
+           'secondary-content' : '#ffffff', /* Foreground content color to use on secondary color */
+
+           'accent' : '#434b54',            /* Accent color */
+           'accent-focus' : '#38403B',      /* Accent color - focused */
+           'accent-content' : '#ffffff',    /* Foreground content color to use on accent color */
+
+           'neutral' : '#3d4451',           /* Neutral color */
+           'neutral-focus' : 'rgb(226 232 240)',     /* Neutral color - focused */
+           'neutral-content' : '#ffffff',   /* Foreground content color to use on neutral color */
+
+           'base-100' : '#ffffff',          /* Base color of page, used for blank backgrounds */
+           'base-200' : '#f9fafb',          /* Base color, a little darker */
+           'base-300' : '#d1d5db',          /* Base color, even more darker */
+           'base-content' : '#1f2937',      /* Foreground content color to use on base color */
+
+           'info' : '#2094f3',              /* Info */
+           'success' : '#009485',           /* Success */
+           'warning' : '#ff9900',           /* Warning */
+           'error' : '#ff5724',             /* Error */
+        },
+      },
+    ],
+  },
+}
