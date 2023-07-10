1672e62a8de25532545dc27739494702df191e2b
stephenkati <stephenkatuli88@gmail.com>
2023-07-10 05:24:02 +0300

    revert changes

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -1,15 +1,30 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   content: [
+    './public/*.html',
     './app/helpers/**/*.rb',
     './app/javascript/**/*.js',
-    './app/views/**/*',
+    './app/views/**/*.{erb,haml,html,slim}'
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/container-queries'),
     require("daisyui"),
   ],
+  // daisyui: {
+  //   themes: ["lemonade"],
+  //   // darkTheme: "dark",
+  // }
   daisyui: {
     themes: [
       {
